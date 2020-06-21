e6d5bcb0b87a18c2f5287862399c39e32ec63f2c
yanglh <1477868882@qq.com>
2020-06-21 23:24:05 +0800

    解决冲突

--- studyGit/src/day01/gitDemo.java
+++ studyGit/src/day01/gitDemo.java
@@ -5,11 +5,7 @@ public class gitDemo {
 	public static void main(String[] args) {
 		//sssss
 		//第四次提交
-<<<<<<< HEAD
-		//本地改的
-=======
-		//远端改的。。
->>>>>>> branch 'master' of https://github.com/yanglonghui-dev/eclispsegitremot.git
+		//本地改的+远端改的
 
 	}
 
